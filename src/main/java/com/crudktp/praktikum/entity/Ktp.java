fe55f4ec6182848717570a511181638474c859a5
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-15 08:17:50 +0700

    membuat package entity

---
+++ src/main/java/com/crudktp/praktikum/entity/Ktp.java
@@ -0,0 +1,30 @@
+package com.crudktp.praktikum.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "Ktp")
+public class Ktp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nomorKtp;
+
+    @Column(nullable = false)
+    private String namaLengkap;
+
+    @Column(nullable = false)
+    private String alamat;
+
+    @Column(nullable = false)
+    private LocalDate tanggalLahir;
+
+    @Column(nullable = false)
+    private String jenisKelamin;
+}
