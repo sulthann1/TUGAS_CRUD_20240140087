974d8cb326eed6771b7c15ed164ad9ba5b226891
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-15 08:22:18 +0700

    membuat package model

---
+++ src/main/java/com/crudktp/praktikum/model/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.crudktp.praktikum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+}
